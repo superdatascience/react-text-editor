f13c9319007e45463d8e210064c081b17e7f1092
Juan Pablo Mejia <juanpmd@hotmail.com>
2020-06-17 19:43:52 -0500

    Create boilerplate npm react package

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import babel from '@rollup/plugin-babel';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { terser } from "rollup-plugin-terser";
+import pkg from './package.json'
+
+export default {
+	input: 'src/index.js',
+	output: [
+		{
+			file: pkg.main,
+			format: 'cjs'
+		}
+	],
+	plugins: [
+		babel({
+			exclude: 'node_modules/**',
+			presets: ['@babel/preset-react']
+		}),
+		resolve(),
+		commonjs(),
+		terser()
+	],
+	external: ['react', 'react-dom']
+}
