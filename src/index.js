7e9653840778b35704ab5676545f49612d84f5d6
sebaslh12 <sebaslh12@gmail.com>
2020-06-19 12:11:54 -0500

    Adding basic storybook conf.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
 
 const ExampleComponent = () => {
 	const [count, setCount] = useState(0);
@@ -8,6 +9,7 @@ const ExampleComponent = () => {
 			<p>Currently, the count is: {count}</p>
 			<button onClick={() => setCount(count - 1)}>Subtract</button>
 			<button onClick={() => setCount(count + 1)}>Add</button>
+			<button onClick={action('clicked')}>I'm an action button</button>
 		</div>
 	);
 }
---
+++ stories/ExampleStory.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ExampleComponent from '../src/index';
+
+export default { title: 'Example' };
+
+export const Example = () => <ExampleComponent />
