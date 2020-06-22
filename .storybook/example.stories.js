ca234d3c732a6556d9649455d29a34918f1d2048
Juan Pablo Mejia <juanpmd@hotmail.com>
2020-06-22 02:13:54 -0500

    Add Editor basic functionality
    This adds the functions of bold, italic, underline, strikethrough.

--- .storybook/example.stories.js
+++ .storybook/example.stories.js
@@ -1,6 +1,25 @@
-import React from 'react';
-import ExampleComponent from '../src/index';
+import React, { useState, Fragment } from 'react';
+import Editor from '../src/index';
 
 export default { title: 'ExampleComponent' };
 
-export const Example = () => <ExampleComponent />
+export const Example = () => {
+	const [values, setValues] = useState({ text: '<p>This is our rich text editor and here we will display different options and functionalities</p><p>There are some elements like bold, italic, underline, strikethrough</p>' });
+
+	const handleChange = (event) => {
+		const { name, value } = event.target;
+		setValues({ ...values, [name]: value });
+	}
+
+	return (
+		<Fragment>
+			<Editor
+				field="text"
+				html={values.text}
+				saveCallback={handleChange}
+				placeholder="Temporal placeholder..."
+			/>
+			<button onClick={() => console.log(values.text)}>Preview Text</button>
+		</Fragment>
+	);
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import ActionButton from './button.js';
+import { BoldIcon, ItalicIcon, UnderlineIcon, StrikethroughIcon, IconEdit, IconCode } from './icons.js';
 
-const ExampleComponent = () => {
-	const [count, setCount] = useState(0);
+const Editor = ({ field, html, saveCallback, placeholder }) => {
+	const [htmlEditor, setHtmlEditor] = useState(false);
+	const editorRef = useRef();
+
+	const handleSave = (event) => {
+		const initialHtml = html;
+		const el = editorRef.current;
+		const newHtml = el ? el.innerHTML : event.target.value;
+
+		if (initialHtml !== newHtml) {
+			const newEvent = Object.assign({}, event, {
+				target: { name: field, value: newHtml }
+			});
+			saveCallback(newEvent);
+		}
+	}
 
 	return (
 		<div>
-			<p>Currently, the count is: {count}</p>
-			<button onClick={() => setCount(count - 1)}>Subtract</button>
-			<button onClick={() => setCount(count + 1)}>Add</button>
+			<ActionButton cmd="bold" icon={BoldIcon} />
+			<ActionButton cmd="italic" icon={ItalicIcon} />
+			<ActionButton cmd="underline" icon={UnderlineIcon} />
+			<ActionButton cmd="strikeThrough" icon={StrikethroughIcon} />
+
+			<span onClick={() => setHtmlEditor(!htmlEditor)}>
+				{htmlEditor ? <IconEdit/> : <IconCode/>}
+			</span>
+			<hr/>
+
+			{htmlEditor ? (
+				<textarea
+					defaultValue={html}
+					placeholder="Write your html code here"
+					onBlur={handleSave}
+				/>
+			) : (
+				<div
+					ref={editorRef}
+					dangerouslySetInnerHTML={{__html: html}}
+					contentEditable={true}
+					placeholder={placeholder ? placeholder : 'Write your rich text here'}
+					onBlur={handleSave}
+				/>
+			)}
 		</div>
 	);
 }
 
-export default ExampleComponent;
+export default Editor;
