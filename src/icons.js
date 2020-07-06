ff8a11a09ab1af876a921f933cd8963e78cdb07e
Juan Pablo Mejia <juanpmd@hotmail.com>
2020-07-06 18:38:11 -0500

    Add styles and options to the editor

--- src/icons.js
+++ src/icons.js
@@ -8,6 +8,22 @@ export const UnderlineIcon = () => <svg height="24" viewBox="0 0 24 24" width="2
 
 export const StrikethroughIcon = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M6.85,7.08C6.85,4.37,9.45,3,12.24,3c1.64,0,3,0.49,3.9,1.28c0.77,0.65,1.46,1.73,1.46,3.24h-3.01 c0-0.31-0.05-0.59-0.15-0.85c-0.29-0.86-1.2-1.28-2.25-1.28c-1.86,0-2.34,1.02-2.34,1.7c0,0.48,0.25,0.88,0.74,1.21 C10.97,8.55,11.36,8.78,12,9H7.39C7.18,8.66,6.85,8.11,6.85,7.08z M21,12v-2H3v2h9.62c1.15,0.45,1.96,0.75,1.96,1.97 c0,1-0.81,1.67-2.28,1.67c-1.54,0-2.93-0.54-2.93-2.51H6.4c0,0.55,0.08,1.13,0.24,1.58c0.81,2.29,3.29,3.3,5.67,3.3 c2.27,0,5.3-0.89,5.3-4.05c0-0.3-0.01-1.16-0.48-1.94H21V12z"/></svg>;
 
+export const IconOrderedList = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M2 17h2v.5H3v1h1v.5H2v1h3v-4H2v1zm1-9h1V4H2v1h1v3zm-1 3h1.8L2 13.1v.9h3v-1H3.2L5 10.9V10H2v1zm5-6v2h14V5H7zm0 14h14v-2H7v2zm0-6h14v-2H7v2z"/></svg>;
+
+export const IconUnorderedList = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M4 10.5c-.83 0-1.5.67-1.5 1.5s.67 1.5 1.5 1.5 1.5-.67 1.5-1.5-.67-1.5-1.5-1.5zm0-6c-.83 0-1.5.67-1.5 1.5S3.17 7.5 4 7.5 5.5 6.83 5.5 6 4.83 4.5 4 4.5zm0 12c-.83 0-1.5.68-1.5 1.5s.68 1.5 1.5 1.5 1.5-.68 1.5-1.5-.67-1.5-1.5-1.5zM7 19h14v-2H7v2zm0-6h14v-2H7v2zm0-8v2h14V5H7z"/></svg>;
+
+export const IconJustifyLeft = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M15 15H3v2h12v-2zm0-8H3v2h12V7zM3 13h18v-2H3v2zm0 8h18v-2H3v2zM3 3v2h18V3H3z"/></svg>;
+
+export const IconJustifyCenter = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M7 15v2h10v-2H7zm-4 6h18v-2H3v2zm0-8h18v-2H3v2zm4-6v2h10V7H7zM3 3v2h18V3H3z"/></svg>;
+
+export const IconJustifyRight = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M3 21h18v-2H3v2zm6-4h12v-2H9v2zm-6-4h18v-2H3v2zm6-4h12V7H9v2zM3 3v2h18V3H3z"/></svg>;
+
+export const IconQuote = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M6 17h3l2-4V7H5v6h3zm8 0h3l2-4V7h-6v6h3z"/></svg>;
+
+export const IconLink = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M3.9 12c0-1.71 1.39-3.1 3.1-3.1h4V7H7c-2.76 0-5 2.24-5 5s2.24 5 5 5h4v-1.9H7c-1.71 0-3.1-1.39-3.1-3.1zM8 13h8v-2H8v2zm9-6h-4v1.9h4c1.71 0 3.1 1.39 3.1 3.1s-1.39 3.1-3.1 3.1h-4V17h4c2.76 0 5-2.24 5-5s-2.24-5-5-5z"/></svg>;
+
+export const IconImage = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M21 19V5c0-1.1-.9-2-2-2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2zM8.5 13.5l2.5 3.01L14.5 12l4.5 6H5l3.5-4.5z"/></svg>;
+
 export const IconEdit = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>;
 
 export const IconCode = () => <svg height="24" viewBox="0 0 24 24" width="24"><path d="M9.4 16.6L4.8 12l4.6-4.6L8 6l-6 6 6 6 1.4-1.4zm5.2 0l4.6-4.6-4.6-4.6L16 6l6 6-6 6-1.4-1.4z"/></svg>;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React, { useState, useRef } from 'react';
 import ActionButton from './button.js';
-import { BoldIcon, ItalicIcon, UnderlineIcon, StrikethroughIcon, IconEdit, IconCode } from './icons.js';
+import { BoldIcon, ItalicIcon, UnderlineIcon, StrikethroughIcon, IconOrderedList, IconUnorderedList, IconJustifyLeft, IconJustifyCenter, IconJustifyRight, IconQuote, IconLink, IconImage, IconEdit, IconCode } from './icons.js';
+import './styles.css';
 
 const Editor = ({ field, html, saveCallback, placeholder }) => {
 	const [htmlEditor, setHtmlEditor] = useState(false);
 	const editorRef = useRef();
+
+	const generateLink = (url) => {
+		const pattern = /^((http|https|ftp):\/\/)/;
+		return !pattern.test(url) ? `https://${url}` : url
+	}
+
+	const handlePaste = (event) => {
+		event.preventDefault();
+		const text = event.clipboardData.getData('text/plain');
+		document.execCommand('insertHTML', false, text);
+	}
 
 	const handleSave = (event) => {
 		const initialHtml = html;
@@ -20,19 +32,44 @@ const Editor = ({ field, html, saveCallback, placeholder }) => {
 	}
 
 	return (
-		<div>
-			<ActionButton cmd="bold" icon={BoldIcon} />
-			<ActionButton cmd="italic" icon={ItalicIcon} />
-			<ActionButton cmd="underline" icon={UnderlineIcon} />
-			<ActionButton cmd="strikeThrough" icon={StrikethroughIcon} />
+		<div className="editable-wrapper">
+			<div className="editable-actions">
+				<ActionButton cmd="bold" icon={BoldIcon} />
+				<ActionButton cmd="italic" icon={ItalicIcon} />
+				<ActionButton cmd="underline" icon={UnderlineIcon} />
+				<ActionButton cmd="strikeThrough" icon={StrikethroughIcon} />
 
-			<span onClick={() => setHtmlEditor(!htmlEditor)}>
-				{htmlEditor ? <IconEdit/> : <IconCode/>}
-			</span>
+				<ActionButton cmd="insertOrderedList" icon={IconOrderedList} />
+				<ActionButton cmd="insertUnorderedList" icon={IconUnorderedList} />
+
+				<ActionButton cmd="justifyLeft" icon={IconJustifyLeft} />
+				<ActionButton cmd="justifyCenter" icon={IconJustifyCenter} />
+				<ActionButton cmd="justifyRight" icon={IconJustifyRight} />
+
+				<ActionButton cmd="formatBlock" icon={IconQuote} arg="blockquote" />
+
+				<ActionButton icon={IconLink} func={() => {
+					const linkUrl = prompt("Enter the URL");
+					var selection = document.getSelection();
+					document.execCommand("createLink", false, generateLink(linkUrl));
+					selection.anchorNode.parentElement.target = '_blank';
+					selection.anchorNode.parentElement.rel = 'noopener';
+				}} />
+
+				<ActionButton icon={IconImage} func={() => {
+					const imageUrl = prompt("Paste image url here");
+					document.execCommand("insertHTML", false, `<img src="${imageUrl}">`);
+				}} />
+
+				<span onClick={() => setHtmlEditor(!htmlEditor)}>
+					{htmlEditor ? <IconEdit/> : <IconCode/>}
+				</span>
+			</div>
 			<hr/>
 
 			{htmlEditor ? (
 				<textarea
+					className="editable-source"
 					defaultValue={html}
 					placeholder="Write your html code here"
 					onBlur={handleSave}
@@ -40,10 +77,12 @@ const Editor = ({ field, html, saveCallback, placeholder }) => {
 			) : (
 				<div
 					ref={editorRef}
+					className="editable-editor"
 					dangerouslySetInnerHTML={{__html: html}}
 					contentEditable={true}
 					placeholder={placeholder ? placeholder : 'Write your rich text here'}
 					onBlur={handleSave}
+					onPaste={handlePaste}
 				/>
 			)}
 		</div>
